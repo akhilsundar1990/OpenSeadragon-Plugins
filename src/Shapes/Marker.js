47b0ca30fd6bb30bddb73b6fd0657235358e8160
David Gutman <dagutman@gmail.com>
2013-03-03 04:18:41 -0500

    Removed No5. from namespace to make calls just openseajax

--- src/Shapes/Marker.js
+++ src/Shapes/Marker.js
@@ -1,14 +1,14 @@
-No5.OpenSeajax.Shapes.Marker = function(src) {
+OpenSeajax.Shapes.Marker = function(src) {
    this.img = document.createElement("img");
    this.img.src = src;
 }
 
-No5.OpenSeajax.Shapes.Marker.prototype.attachTo = function(viewer, x, y) {
-   var pt = No5.OpenSeajax.toWorldCoordinates(viewer, x, y);
+OpenSeajax.Shapes.Marker.prototype.attachTo = function(viewer, x, y) {
+   var pt = OpenSeajax.toWorldCoordinates(viewer, x, y);
    viewer.drawer.addOverlay(this.img, pt, Seadragon.OverlayPlacement.BOTTOM);   
 }
 
-No5.OpenSeajax.Shapes.Marker.prototype.addEventListener = function(evt, listener) {
+OpenSeajax.Shapes.Marker.prototype.addEventListener = function(evt, listener) {
    Seadragon.Utils.addEvent(this.img, evt, Seadragon.Utils.stopEvent);
    this.img.addEventListener(evt, listener, false);
 }
